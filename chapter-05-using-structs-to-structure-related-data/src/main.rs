8b055fe15766c9df4b070b6411bee9ea71bf5487
Philippe Vinchon <p.vinchon@gmail.com>
2018-12-09 17:42:05 +0000

    Chapter 05

---
+++ chapter-05-using-structs-to-structure-related-data/src/main.rs
@@ -0,0 +1,72 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+fn main() {
+    let width = 30;
+    let height = 50;
+    let area = area1(width, height);
+
+    let rect = (30, 50);
+    let area = area2(rect);
+
+    let rect = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let area = area3(&rect);
+    let area = rect.area();
+
+    println!("The area of the rectangle is {} square pixels.", area);
+    println!("rect is {:?}.", rect);
+
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let square = Rectangle::square(10);
+    println!("rect is {:?}.", square);
+}
+
+fn area1(width: u32, height: u32) -> u32 {
+    width * height
+}
+
+fn area2(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
+
+fn area3(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
